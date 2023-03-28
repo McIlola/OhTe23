7dae7885dc4692a14762959a363245c117022a29
Matias Ilola <matias.ilola@helsinki.fi>
2023-03-28 17:08:42 +0300

    uusi

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,20 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_kortille_voi_ladata_rahaa(self):
+        self.maksukortti.lataa_rahaa(2500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 35.00 euroa")
+
+    def test_ota_rahaa_vahenee_oikein(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+        self.assertTrue(self.maksukortti.ota_rahaa(50),"Kortilla on rahaa 4.50 euroa")
+    
+    def test_ota_rahaa_vahenee_oikein2(self):
+        self.maksukortti.ota_rahaa(2500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+        self.assertFalse(self.maksukortti.ota_rahaa(2500),"Kortilla on rahaa 10.00 euroa")
