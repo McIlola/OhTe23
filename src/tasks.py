1842e888956ff39065a2841f76fb315eba6e5e99
Matias Ilola <matias.ilola@helsinki.fi>
2023-04-25 13:52:39 +0300

    siirto

--- src/tasks.py
+++ src/tasks.py
@@ -15,7 +15,3 @@ def coverage(ctx):
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
-
-@task
-def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest", pty=True)
